da89da9d0a9a9a21e17065fffb0511743e941a50
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-03-17 20:42:54 -0700

    added/models user code

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const { model, DaataTypes} = require('sequelize');
+const { Model, DataTypes} = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
