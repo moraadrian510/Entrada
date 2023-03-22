5b815c683f39018377db008fc5100a1e557ff1a7
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-03-21 20:09:53 -0700

    updated user.mode

--- models/User.js
+++ models/User.js
@@ -39,8 +39,14 @@ User.init (
     {
         hooks: {
             beforeCreate: async (newUserData) => {
-                newUserData.password = await bcrypt.hash(newUserData.password, 10)
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
             },
+
+            beforeUpdate: async (updatedUserData) => {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+              },
         },
         sequelize,
         timestamps: false,
--- models/post.js
+++ models/post.js
@@ -1,48 +1,45 @@
-const { Model, DataTypes} = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Post extends Model {}
 
-Post.init (
-    {
-        id: {
-            type: DataTypes.STRING,
-            allowNull:false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull:false,
-            validate: {
-                len: [1]
-            }
-        },
-        content:{
-            type: DataTypes.STRING,
-            allowNull:false,
-            validate : {
-                len: [1]
-            }
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'user',
-                key: 'id',
-            }
-
-        }
-
+Post.init(
+{
+    id: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
     },
-    {
-     
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'post', 
-    }
+    title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+        len: [1],
+    },
+    },
+    content: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+        len: [1],
+    },
+    },
+    user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+        model: "user",
+        key: "id",
+    },
+    },
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
+}
 );
 
 module.exports = Post;
