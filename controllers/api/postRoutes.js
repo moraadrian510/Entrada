650fb9ee8af775e6b0d6520c8c50dc9ee55bf938
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-03-21 21:02:20 -0700

    added post routes

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const { Post } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+router.post("/", withAuth, async (req, res) => {
+try {
+    const newPost = await Post.create({
+    ...req.body,
+      //  property is set to the ID of the user who is currently logged in
+    user_id: req.session.user.id,
+    });
+
+    res.status(200).json(newPost);
+} catch (err) {
+    res.status(404).json(err);
+}
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const commentData = await Comment.destroy({
+            where: {
+                id: req.session.user.id,
+                user_id: req.session.user.id
+            }
+        })
+    } catch (err) {
+        res.status(404).json(err);
+    };
+});
+
+
+module.exports = router;
