86e986eb523c7fb9bb35dec068427c8b2bb82d63
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-03-22 15:45:08 -0700

    added cooment route

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,8 +1,67 @@
 const router = require("express").Router();
-const { Comment } = require("../../models");
+const { Comment, Post, User } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-router.
+// create new post 
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const commentData = await Comment.create({
+            comment_text: req.body.comment_text
+        });
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(404).json(err);
+    }
+});
 
+
+//Get all comments
+router.get("/", async (req, res) => {
+  try {
+    const commentData = await Comment.findAll();
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
+//Get a single comment
+router.get("/:id", async (req, res) => {
+  try {
+    const commentData = await Comment.findByPk(req.params.id);
+    if (!commentData) {
+      res.status(404).json({ message: "No user with that id found!" });
+    }
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
+//Update a comment
+router.put('/:id', withAuth,  async (req, res) => {
+    try {
+        const commentData = await Comment.update(req.body, {
+            where: {id: req.params.id, user_id: req.session.userId },
+        });
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(404).json(err)
+    }
+});
+
+//Delete a comment
+router.delete('/:id', withAuth, async (req,res) => {
+    try {
+        const commentData = await Comment.destroy({
+            where: {
+                id: req.session.comment.id,
+            },
+        });
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(404).json(err)
+    }
+});
 
 module.exports = router;
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,51 +2,51 @@ const router = require("express").Router();
 const { Post, Comment, User } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-//get a single post 
-router.get('/:id', withAuth, async (req, res) => {
-    try {
-        const postData = await Post.findByPk(req.params.id, {
-            include: [{model: User }, {model: Comment }],
-            attributes: {
-                exclude: ['password']
-            }
-        })
-        res.status(200).json(postData);
-    } catch (err) {
-        res.status(404).json()
-    }
-})
+//get a single post
+router.get("/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [{ model: User }, { model: Comment }],
+      attributes: {
+        exclude: ["password"],
+      },
+    });
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(404).json();
+  }
+});
 
 //get all posts
-router.get('/', withAuth, async (req, res) => {
-    try {
-        const postData = await Post.findAll({
-            include: [{ model: Comment}, { model: User}],
-            attributes: {
-                exclude : ['password']
-            }
-        });
-        res.status(200).json(postData);
-    } catch (err) {
-        res.status.json(err)
-    };
-})
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [{ model: Comment }, { model: User }],
+      attributes: {
+        exclude: ["password"],
+      },
+    });
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status.json(err);
+  }
+});
 
 //update post
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
     try {
-        const postData = await Post.update(req.body, {
-            where: {
-                id: req.session.user.id
-            },
-        });
-
-        if (!postData) {
-            res.status(404).json({ message: 'Error updating post, post with this id not found!' });
-        };
-        res.status(200).json(postData);
+    const postData = await Post.update(req.body, { where: {
+        id: req.session.user.id,
+    },
+    });
+    if (!postData) {
+    res
+        .status(404)
+        .json({ message: "Error updating post, post with this id not found!" });
+    }
+    res.status(200).json(postData);
     } catch (err) {
-        res.status(404).json()
+    res.status(404).json();
     }
 });
 
