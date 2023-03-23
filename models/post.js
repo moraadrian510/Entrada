0a6b777826d78bf789f579cb38cdde2c8cb9cb13
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-03-23 15:35:54 -0700

    save before bug fix

--- models/post.js
+++ models/post.js
@@ -25,6 +25,11 @@ Post.init(
         len: [1],
     },
     },
+    date_created: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    },
     user_id: {
     type: DataTypes.INTEGER,
     references: {
